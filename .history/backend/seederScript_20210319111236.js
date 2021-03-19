6b0e1308a29a998467016968c5b09829db476148
kalden <tseringkalden1964@gmail.com>
2021-03-19 11:16:42 -0700

    setting up the backend with mongoDB

---
+++ .history/backend/seederScript_20210319111236.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const productsData = require('./data/products');
+const connectDB = require('./config/db');
+const Product = require('./models/Product');
+
+connectDB();
+
+const importData = () => 
+]
